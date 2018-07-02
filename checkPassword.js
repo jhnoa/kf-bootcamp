8e3db168e88e482c6c22a71a9affaa6420b426b2
deandrasita <Me@deandra.lan>
2018-07-02 12:06:42 +0700

    added checkpassword

--- checkPassword.js
+++ checkPassword.js
@@ -1,8 +1,29 @@
+/* 
+1) length must be > 6
+2) must have uppercase & lowercase
+*/ 
+
 function checkPassword(input){
 
-    let result = true; 
+    function hasLowerCase(input) {
+        return (/[a-z]/.test(input));
+    }
+
+    function hasUpperCase(input){
+        return (/[A-Z]/.test(input))
+    }
 
     if (input.length < 6){
-        result = false
+         return  false;
     }
+    else {
+        if (hasLowerCase(input) === false && hasUpperCase(input) === false) {
+             return false
+        }
+    }
+
+    return true
 }
+
+let input = 'passWord'
+console.log(checkPassword(input))
